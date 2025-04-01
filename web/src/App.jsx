792cc05a1bb965bf22413efcdf5efee36ef279c2
Américo <84201638+AmericoRoca@users.noreply.github.com>
2025-04-01 22:12:36 +0200

    sending changes to main

---
+++ web/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import { Carga } from './cargaPagina/carga'
+
+
+function App() {
+
+
+  return (
+    <>
+     <Carga/>
+    </>
+  )
+}
+
+export default App
