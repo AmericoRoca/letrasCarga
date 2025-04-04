378f7b84afde754bec29c00dc8d08d576a69a8e8
Américo <americochiclana@gmail.com>
2025-04-04 21:28:21 +0200

    cambiar srtilos enlaces!

--- web/src/Navbar/Navbar.jsx
+++ web/src/Navbar/Navbar.jsx
@@ -1,34 +1,47 @@
-import React from 'react'
-import "../Navbar/Navbar.css"
-import { LogoSvg } from "../assets/img/logo/LogoSvg.jsx"
+import React from "react";
+import "../Navbar/Navbar.css";
+import { LogoSvg } from "../assets/img/logo/LogoSvg.jsx";
 import gsap from "gsap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHome,
+  faUser,
+  faCode,
+  faAt,
+  faTools,
+} from "@fortawesome/free-solid-svg-icons";
 
 export const Navbar = () => {
-
-    // const animationLogo = () => {
-    //     let tl = gsap.timeline(); condicion que termine animacion carga
-    //     tl.set(".logo-navbar", {duration: 0.5, y:20, delay:0.5})
-    //     tl.to(".logo-navbar", {duration: 0.5, y:0, delay:0.5})
-    // }
+  // const animationLogo = () => {
+  //     let tl = gsap.timeline(); condicion que termine animacion carga
+  //     tl.set(".logo-navbar", {duration: 0.5, y:20, delay:0.5})
+  //     tl.to(".logo-navbar", {duration: 0.5, y:0, delay:0.5})
+  // }
 
   return (
-
-    <div className='navbar'>
-    <div className='caja'>
-        <div className='logo-navbar'>
-            <LogoSvg />
-        </div>
-    </div>
-    <div className='caja'>
-        <ul>
-            <li className="text-white">1</li>
-            <li className="text-white">2</li>
-            <li className="text-white">3</li>
-            <li className="text-white">4</li>
-            <li className="text-white">5</li>
+    <div className="navbar flex flex-col items-center py-4 h-[100px]">
+      <div className="w-full flex justify-center logo mb-4">
+        <LogoSvg />
+      </div>
+      <div className="div-ul w-full flex justify-center">
+        <ul className="flex flex-col gap-6 items-center pl-0">
+          <li className="text-white">
+            <a href=""><FontAwesomeIcon icon={faHome} /></a>
+          </li>
+          <li className="text-white">
+           <a href=""> <FontAwesomeIcon icon={faUser} /></a>
+          </li>
+          <li className="text-white">
+            <a href=""><FontAwesomeIcon icon={faTools} /></a>
+          </li>
+          <li className="text-white">
+            <a href=""><FontAwesomeIcon icon={faCode} /></a>
+          </li>
+          <li className="text-white">
+           <a href=""><FontAwesomeIcon icon={faAt} /></a> 
+          </li>
         </ul>
+      </div>
     </div>
-</div>
-
-  )
-}
+  );
+};
