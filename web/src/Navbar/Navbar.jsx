d4e813fd142955ba5959e320befa85985f1d5fd8
Américo <84201638+AmericoRoca@users.noreply.github.com>
2025-04-04 12:27:36 +0200

    navbar

--- web/src/Navbar/Navbar.jsx
+++ web/src/Navbar/Navbar.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import "../Navbar/Navbar.css"
 import { LogoSvg } from "../assets/img/logo/LogoSvg.jsx"
+import gsap from "gsap";
 
 export const Navbar = () => {
+
+    // const animationLogo = () => {
+    //     let tl = gsap.timeline(); condicion que termine animacion carga
+    //     tl.set(".logo-navbar", {duration: 0.5, y:20, delay:0.5})
+    //     tl.to(".logo-navbar", {duration: 0.5, y:0, delay:0.5})
+    // }
+
   return (
 
-    <div className='navbar d-flex flex-column'>
+    <div className='navbar'>
     <div className='caja'>
         <div className='logo-navbar'>
             <LogoSvg />
